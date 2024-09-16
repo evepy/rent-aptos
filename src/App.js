10a023cb116d3b8b41bd51a5618ad3a5a47c7854
deveiap <85376092+deveiap@users.noreply.github.com>
2024-09-15 22:31:13 -0300

    aptos sdk

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Account, Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk";
+//npm install @aptos-labs/ts-sdk
+
+const config = new AptosConfig({ network: Network.TESTNET });
+const aptos = new Aptos(config);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hola Mundo
     </div>
   );
 }
